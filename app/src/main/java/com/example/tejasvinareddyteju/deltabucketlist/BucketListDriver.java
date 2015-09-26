b46c7e88195285396882e5ac0e0e76e2ee0d63a9
Tejasvi Nareddy <tejunareddy@gmail.com>
2015-09-26 18:29:36 -0400

    Revert "Bu"
    This reverts commit e0700a2c98a7569ed09ab13ad2f8f4a767ada31f.

--- app/src/main/java/com/example/tejasvinareddyteju/deltabucketlist/BucketListDriver.java
+++ app/src/main/java/com/example/tejasvinareddyteju/deltabucketlist/BucketListDriver.java
@@ -6,39 +6,7 @@ package com.example.tejasvinareddyteju.deltabucketlist;
 import java.util.ArrayList;
 public class BucketListDriver {
     ArrayList<Destination> bucketList = new ArrayList<Destination>();
-    ArrayList<Destination> possibleDestinations = new ArrayList<Destination>();
-
-    public BucketListDriver() {
-        possibleDestinations.add(new Destination("New York City"));
-        possibleDestinations.add(new Destination("Atlanta"));
-        possibleDestinations.add(new Destination("Los Angelas"));
-        possibleDestinations.add(new Destination("Dallas"));
-        possibleDestinations.add(new Destination("Las Vegas"));
-    }
-
     public void addDestination(Destination place){
         bucketList.add(place);
-    }
-
-    public Destination findDestinationByName(String name) {
-        for (Destination d: bucketList) {
-            if (d.getName().equals(name)) {
-                return d;
-            }
-        }
-
-        return null;
-    }
-
-    public Destination[] toArray() {
-        return (Destination[]) bucketList.toArray();
-    }
-
-    public void removeDestination(String name) {
-        for(int i = 0; i < bucketList.size(); i++) {
-            if( bucketList.get(i).equals(name)) {
-                bucketList.remove(i);
-            }
-        }
     }
 }
